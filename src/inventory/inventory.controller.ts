70ca72a37309b12fa42794b34ea123e5aa7e5a64
Pawel Cwik <cwiku86@gmail.com>
2019-10-19 11:01:20 +0200

    Prettire and lint

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -1,40 +1,46 @@
-import { Controller, Get, Post, Body, Param, Put, Delete } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from "@nestjs/common";
 import { InventoryRepository } from "./inventory.repository";
 import { BookDTO } from "./domain/dto/BookDTO";
 import { get } from "https";
 
 @Controller("/inventory")
 export class InventoryController {
+  private repository: InventoryRepository;
 
-    private repository : InventoryRepository;
+  constructor(repo: InventoryRepository) {
+    this.repository = repo;
+  }
 
-    constructor(repo : InventoryRepository) {
-        this.repository = repo;
-    }
+  @Post()
+  public createNewBook(@Body() bookDTO: BookDTO): void {
+    this.repository.createBook(bookDTO);
+  }
 
-    @Post()
-    public createNewBook(@Body() bookDTO : BookDTO) : void {
-        this.repository.createBook(bookDTO);
-    }
+  @Get()
+  public getAllBooks(): BookDTO[] {
+    return this.repository.getAllBooksDTO();
+  }
 
-    @Get()
-    public getAllBooks() : BookDTO[] {
-        return this.repository.getAllBooksDTO();
-    }
+  @Get("/:id")
+  public getBook(@Param("id") id: string): BookDTO {
+    return this.repository.getBookById(Number.parseInt(id));
+  }
 
-    @Get("/:id")
-    public getBook(@Param("id") id : string) : BookDTO {
-        return this.repository.getBookById(Number.parseInt(id));
-    }
+  @Put("/:id")
+  public updateBook(@Param("id") id: string, @Body() bookDTO: BookDTO): void {
+    this.repository.updateBook(Number.parseInt(id, 10), bookDTO);
+  }
 
-    @Put("/:id")
-    public updateBook(@Param("id") id : string, @Body() bookDTO: BookDTO) : void {
-        this.repository.updateBook(Number.parseInt(id), bookDTO);
-    }
-
-    @Delete("/:id")
-    public deleteBook(@Param("id") id : string) : void {
-        this.repository.deleteBook(Number.parseInt(id));
-    }   
-
+  @Delete("/:id")
+  public deleteBook(@Param("id") id: string): void {
+    this.repository.deleteBook(Number.parseInt(id, 10));
+  }
 }
